8e5873129e0cdd837f502219158c79574beec36c
luiz-prog <111691274+luiz-prog@users.noreply.github.com>
2024-03-13 23:04:31 -0300

    Trabalho.WMS

--- WMS.py
+++ WMS.py
@@ -1,17 +1,14 @@
 lista = list()
-produto = dict()
 
-def salvar():
+
+
+def salvar(produto):
     lista.append(produto.copy())
-
-def comentario():
-    print()
 
 def validar_codigo(codigo):
     for item in lista:
         if codigo == item['codigo']:
             return True
-    return False
 
 def visualizar():
     for i in lista:
@@ -20,25 +17,27 @@ def visualizar():
 def cadastro():
     print()
     print('Etapa de realizar o cadastramento do produto.')
-    while(True):    
-            print("Tela de cadastro, por gentileza preencher as informções abaixo:")
-            produto['codigo'] = (input('Digite o código do item que irá cadastrar: '))
-            while validar_codigo(produto['codigo']): 
-                produto['codigo'] = str(input('Código já existe, favor informar outro: '))
-            produto['tamanho'] = str(input('Digite as dimenções do produto no formato : '))
-            salvar()
-            if (int(input("Deseja continuar? {0} sim {1} não")) == 1): break
+    while(True): 
+        produto = {'codigo':"", 'tamanho':"", 'comentario':[]}  
+        print("Tela de cadastro, por gentileza preencher as informções abaixo:")
+        produto['codigo'] = (input('Digite o código do item que irá cadastrar: '))
+        while validar_codigo(produto['codigo']): 
+            produto['codigo'] = str(input('Código já existe, favor informar outro: '))
+        produto['tamanho'] = str(input('Digite as dimenções do produto no formato LxAxP (LARGURAxALTURAxPROFUNDIDADE): '))
+        while(True):
+            comentario = (input('Digite um comentario ao item: '))
+            produto['comentario'].append(comentario)
+            if (int(input("Deseja adicionar mais comentários? {0} sim {1} não")) == 1): break 
+        salvar(produto)
+        if (int(input("Deseja cadastrar mais um item? {0} sim {1} não")) == 1): break
 
 while(True):
-    opcao = int(input('1-CADASTRAR\n2-ADICIONAR COMENTÁRIOS\n4-VISUALIZAR CADASTROS\n3-SAIR'))
+    opcao = int(input('1-CADASTRAR\n2-VISUALIZAR CADASTROS\n3-SAIR'))
     if (opcao == 1):
         cadastro()
 
-    elif (opcao == 4):
+    elif (opcao == 2):
         visualizar()
-            
-    elif (opcao ==2):
-        comentario()
 
     elif opcao == 3:
         break   
